695426171a9fc079b91998f4e063056b12e5f66f
And1Ray <36099876+And1Ray@users.noreply.github.com>
2018-07-28 23:50:34 +0300

    day2

---
+++ day2/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', function() {
+  
+
+  let chooseBtn = document.getElementById('choose'),
+      receiveBtn = document.getElementById('receive'),
+      heading = document.getElementsByTagName('h2')[0],
+      nameInput = document.getElementsByClassName('contactform_name')[0],
+      phoneInput = document.querySelector('.contactform_phone'),
+      mailInput = document.querySelectorAll('contactform_mail')[0],
+      modal = document.querySelector('.modal'),
+      close = document.querySelector('.close'),
+      text = document.getElementsByName('message')[0];
+
+
+  function hover() {
+    heading.textContent = 'Действительно все!';
+  }
+  function out() {
+    heading.textContent = 'Все включено';
+  }
+  heading.addEventListener('click', hover);
+  heading.addEventListener('dblclick', out);
+
+  receiveBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  nameInput.addEventListener('input', () => {
+    text.value = `Меня зовут ${nameInput.value}. И я хочу спросить:`;
+    if (nameInput.value === '') {
+      text.value = '';
+    }
+  });
+
+});
+
+
+
+  
